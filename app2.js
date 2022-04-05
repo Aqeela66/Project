87da603d83399070db50df7e6b9bdf0001723442
Aqeela <83452367+Aqeela66@users.noreply.github.com>
2022-04-05 12:00:30 +0500

    updated JS file

---
+++ app2.js
@@ -0,0 +1,12 @@
+function Scrolll(){
+    let left=document.querySelector(".scroll-images");
+    left.scrollBy(350, 0)
+
+  }
+  function Scrolllr(){
+    let right=document.querySelector(".scroll-images");
+    right.scrollBy(-350, 0)
+    
+  }
+
+  
